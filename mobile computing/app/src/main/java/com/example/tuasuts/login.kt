68ef1d1e52bfeebff1711d6dcf7b0f97e70bab3f
unknown <kayamebate@gmail.com>
2023-04-27 11:12:40 +0900

    update tugas uts

---
+++ mobile computing/app/src/main/java/com/example/tuasuts/login.kt
@@ -0,0 +1,15 @@
+package com.example.tuasuts
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
